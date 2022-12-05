c2f1bb762718a78a057ce00c592f60598eb69752
chaerin-77 <rosachae12@gmail.com>
2022-12-05 23:50:35 +0900

    test

---
+++ app/src/main/java/com/example/alarmforsleep/SecondFragment.java
@@ -0,0 +1,19 @@
+package com.example.alarmforsleep;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+
+public class SecondFragment extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        ViewGroup rootView = (ViewGroup) inflater.inflate(
+                R.layout.fragment_second, container, false);
+
+        return rootView;
+    }
+}
